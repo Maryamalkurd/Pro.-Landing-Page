4ff4ec331cfb97032df0a69f27826b1452bc92fc
unknown <you@example.com>
2022-10-06 11:12:31 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+//      owl carousel code started here
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:true,
+    // autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        }
+    }
+})
